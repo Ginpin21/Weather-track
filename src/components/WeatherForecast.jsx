0045025e3079abcaddaaa577f65bafe7b071720a
Ginpin21 <iftekharahmed2003@gmail.com>
2022-11-21 20:07:14 +0400

    first commit

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -6,7 +6,7 @@ const context = useContext(appContext)
   return (
     <>
     {context.weather && <ul className="md:w-half flex flex-col justify-center w-full p-5 my-5 rounded-md shadow dark:shadow-slate-200 shadow-slate-500 text-shadow-white dark:text-shadow-black ">
-      <h1 className="text-center text-4xl my-3">{"Weather Forecast for "+context.weather.location.name}</h1>
+      <h1 className="text-center text-4xl my-3">{"Weather Forecast for "+context.weather.location.name.replace(/[^a-zA-Z0-9 ]/g, '')}</h1>
       {context.weather.forecast.forecastday.map(day=>
           <li key={day.date} className="grid dark:bg-slate-800 bg-slate-50 rounded-md grid-cols-6 gap-0 mb-2 p-4 shadow dark:shadow-slate-200 shadow-slate-500">
             <h2 className="md:text-5xl text-2xl col-span-2">{context.celsius?day.day.avgtemp_c+"°C":day.day.avgtemp_f+"°F"}</h2>
